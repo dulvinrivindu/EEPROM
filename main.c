a9c57907411be25c5cbaa11b8a2b6600e833ef68
dulvinrivindu <dulvinrivindu@gmail.com>
2016-05-06 11:59:38 +0530

    Changed EEMEM variables to Global

--- main.c
+++ main.c
@@ -1,16 +1,18 @@
 #include <avr/io.h>
 #include <avr/eeprom.h>
 
+
+//Recipies in the order
+float EEMEM curry1[6] = {4,0.65,0.5,1.5,0.5,0.75};
+float EEMEM curry2[6] = {6,6,3,3,1,2.5};
+float EEMEM curry3[6] = {3,2.5,0.5,0.75,0.5,0};
+float EEMEM curry4[6] = {0.5,1,0.5,1,0,0};
+
 //Define this function to get user input
 int get_input();
 
 int main(){
 
-	//Recipies in the order
-	float EEMEM curry1[6] = {4,0.65,0.5,1.5,0.5,0.75};
-	float EEMEM curry2[6] = {6,6,3,3,1,2.5};
-	float EEMEM curry3[6] = {3,2.5,0.5,0.75,0.5,0};
-	float EEMEM curry4[6] = {0.5,1,0.5,1,0,0};
 	
 	//getting user input
 	int recpNum = get_input();
